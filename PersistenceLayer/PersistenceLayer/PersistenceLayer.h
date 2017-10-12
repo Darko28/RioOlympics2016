ce93efc7f67d0ad0bb2b031916eb1e116f12af61
Darko <qq1572055493@icloud.com>
2017-10-12 15:36:22 +0800

    RioOlympics2016 first commit without workspace

---
+++ PersistenceLayer/PersistenceLayer/PersistenceLayer.h
@@ -0,0 +1,19 @@
+//
+//  PersistenceLayer.h
+//  PersistenceLayer
+//
+//  Created by Darko on 2017/10/12.
+//  Copyright © 2017年 Darko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PersistenceLayer.
+FOUNDATION_EXPORT double PersistenceLayerVersionNumber;
+
+//! Project version string for PersistenceLayer.
+FOUNDATION_EXPORT const unsigned char PersistenceLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PersistenceLayer/PublicHeader.h>
+
+
